9115c7ad3a1412bbb26755ebdb7875c3a133b2ca
Art2B <arthur.battut@gmail.com>
2017-09-08 12:32:15 +0200

    get domains from posts

--- index.js
+++ index.js
@@ -1,10 +1,24 @@
 const moment = require('moment')
 const request = require('axios')
+const _ = require('lodash')
 
 const config = require('./config.json')
 
 let redditPosts = []
 let limitDate = moment(config.limitDate, 'DD-MM-YYYY')
+
+
+function getDomainsFromPosts (posts) {
+  let domains = []
+
+  posts.map(function (post) {
+    if (!_.find(domains, function (n) { return n === post.data.domain })) {
+      domains.push(post.data.domain)
+    }
+  })
+
+  return domains
+}
 
 request.get(config.subredditurl)
 .then(function (response) {
@@ -15,7 +29,8 @@ request.get(config.subredditurl)
     }
   })
 
-  console.log(redditPosts.length)
+  let domainsList = getDomainsFromPosts(redditPosts)
+  console.log(domainsList)
 })
 .catch(function (error) {
   console.log(error)
